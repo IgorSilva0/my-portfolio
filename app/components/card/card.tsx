2e311e68b888369cd4102d24bdc6ef3e06bab4e2
Igor Silva <igorsilvauk@gmail.com>
2024-05-05 18:37:04 +0100

    lint error addressed

--- app/components/card/card.tsx
+++ app/components/card/card.tsx
@@ -1,11 +1,16 @@
 import styles from './styles.module.scss'
+import Image from 'next/image';
 import { FaGithub, FaLinkedinIn, FaXTwitter, FaInstagram } from 'react-icons/fa6'
 
 
 const Card = () => {
     return (
         <div className={styles.imgBg}>
-            <img className={styles.img} src="./imgs/image.png" alt="Profile Image"/>
+            <Image
+            className={styles.img}
+            src="/imgs/image.png" // Adjust the path accordingly
+            alt="Profile Image"
+            />
             <div className={styles.division}/>
             <p>Join my journey</p>
             <ul className={styles.social}>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-import styles from './styles/styles.module.scss'
+import styles from './styles/styles.module.scss';
 
 const Home = () => {
   return (
     <div className={styles.home}>
-      <h1>Hello, I'm Igor Silva,</h1>
+      <h1>Hello, I&apos;m Igor Silva,</h1>
       <p>A passionate Junior Developer, ready to take you along on my exhilarating journey.</p>
       <p>Recently graduate from a bootcamp, I immersed myself in every aspect of tech, collaborating with over 60 peers in a dynamic virtual workspace via Zoom for months.</p>
       <p>Beyond sharpening my technical skills, I embraced teamwork, effective communication, and agile methodologies.</p>
-      <p>Now armed with a diverse skill set and a genuine passion for innovation, I'm excited about joining a company where I can continue learning, explore new languages, and contribute to impactful projects.</p>
-      <p>Thank you for visiting—I'm looking forward to connecting with you soon!</p>
+      <p>Now armed with a diverse skill set and a genuine passion for innovation, I&apos;m excited about joining a company where I can continue learning, explore new languages, and contribute to impactful projects.</p>
+      <p>Thank you for visiting—I&apos;m looking forward to connecting with you soon!</p>
     </div>
   );
 }
