433fcd2efcf2b91f88339297f327b8bf1cfab9c4
Igor Silva <igorsilvauk@gmail.com>
2024-08-12 18:37:16 +0100

    live link added to car rental

--- public/imgs/CarRental/imgs.tsx
+++ public/imgs/CarRental/imgs.tsx
@@ -17,5 +17,6 @@ export const CarRentalQuote = {
     title: 'Advanced Car Rental Management System',
     text: 'Streamline your travel with our Car Rental App, crafted with React and Next.js for reliable performance. Access real-time availability, secure login, and a responsive mobile design. Upcoming features like payment integration and UI/UX improvements will enhance your experience even further. Start your adventure now and stay tuned for more updates!',
     repo: 'https://github.com/IgorSilva0/Car-Rental',
+    live: 'https://car-rental-i88ynlxl1-igorsilva0s-projects.vercel.app/',
     stack: ['SiNextdotjs', 'SiVercel', 'SiGithubactions', 'SiTailwindcss', 'SiSupabase', 'SiTypescript', 'SiSwiper'],
 }
